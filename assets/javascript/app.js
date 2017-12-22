9a49daeb7e5adc0b0d4beaaedaa38f314d54eaf7
unknown <vramkupelian@gmail.com>
2017-12-22 14:14:36 -0800

    Added images for questions and answers.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,60 +7,80 @@ var trivia = [{
     question: "Fry gets cryogenically frozen. What year does he thawed?",
     possibleAnswers: ["2500","3000","3500","4000"],
     answer:1,
+    qImage: $("<img src = assets/images/image0.jpg>"),
+    aImage: $("<img src = assets/images/afterImage0.jpg>"),
     explanation: "Fry was frozen on December 31, 1999 and stayed that way for just over a thousand years before being thawed out in the year 3000."
     },
     {
     question: "Fry lives in New New York where rats have been replaced by a new generation of vermin. Which animal is the new pest?",
     possibleAnswers: ["Cat","Poisonous Froad","Owl","Vampire Slug"],
     answer:2,
+    qImage: $("<img src = assets/images/image1.jpg>"),
+    aImage: $("<img src = assets/images/afterImage1.jpg>"),
     explanation: "The owls were brought in to take care of the rats before they became a problem themselves."
     },
     {
     question:"Hermes Conrad is the Jamaican accountant and bureaucrat for Planet Express. What is his bureaucrat grade (i.e. rank)?",
-    possibleAnswers:["25","34","43","52"],
+    possibleAnswers:["25","36","47","52"],
     answer: 1,
-    explanation: "Though he has an episode where he gets demoted, Hermes eventually regains his rank as a Grade 34 bureaucrat."
+    qImage: $("<img src = assets/images/afterImage2.jpg>"),
+    aImage: $("<img src = assets/images/image2.jpg>"),
+    explanation: "Though his grade has changed a few times, Hermes started and finished the show as a Grade 36 bureaucrat."
     },
     {
     question: "Who is the only intelligent organism on Earth deemed 'nice' by Robot Santa Claus?",
     possibleAnswers: ["Nibbler", "Fry", "Leela", "Zoidberg"],
     answer: 3,
+    qImage: $("<img src = assets/images/image3.jpg>"),
+    aImage: $("<img src = assets/images/afterImage3.jpg>"),
     explanation: "Robot Santa's homicidal programming has made him try to kill everyone, except for Zoidberg whom got gifted a pogo-stick."
     },
     {
     question: "In the show, which character has never said a word but has its own tv show?",
     possibleAnswers: ["Wash bucket", "El Chupanibre", "Hypnotoad", "Pamela Anderson's head in a jar"],
     answer: 2,
+    qImage: $("<img src = assets/images/image4.jpg>"),
+    aImage: $("<img src = assets/images/afterImage4.gif>"),
     explanation: "This reoccuring cameo was frequently on the television hypnotizing its audience."
     },
     {
     question: "How many times did Futurama end?",
     possibleAnswers: ["0", " 1", "2", "3"],
     answer: 3  ,
+    qImage: $("<img src = assets/images/image5.jpg>"),
+    aImage: $("<img src = assets/images/afterImage5.jpg>"),
     explanation: "They show was cancelled three times and even had a couple movies." 
     },
     {    
     question: "What was Fry's good luck charm?",
     possibleAnswers:["Lucky Rabbit Foot", "His Red Jacket", "Slurm", "A Seven Leaf Clover"],
     answer: 3,
+    qImage: $("<img src = assets/images/image6.jpg>"),
+    aImage: $("<img src = assets/images/afterImage6.jpg>"),
     explanation: "He couldn't beat his brother at basketball without it."
     },
     {   
     question: "Which of these inventions was not made by the Professor?",
     possibleAnswers: ["The Mind-Switcher", "The What-If Machine", "The eyePhone","The Smell-O-Scope"],
     answer: 2,
+    qImage: $("<img src = assets/images/image7.gif>"),
+    aImage: $("<img src = assets/images/afterImage7.gif>"),
     explanation: "Though the Professor has made plenty of inventions, the eyePhone is a MomCorp product."
     },
     {
     question:"How many people on the writing staff have either a PhD or a Master's degree?",
     possibleAnswers: ["5", "7", "10", "12"],
     answer: 2,
+    qImage: $("<img src = assets/images/image8.jpg>"),
+    aImage: $("<img src = assets/images/afterImage8.jpg>"),
     explanation: "Three PhDs and seven Masters degrees in fields such as applied mathematics, physics, and computer science."
     },
     {
     question: "The creator of Futurama also created what other animated sitcom?",
     possibleAnswers: ["The Simpsons", "Family Guy", "South Park", "King of the Hill"],
     answer: 0,
+    qImage: $("<img src = assets/images/image9.jpg>"),
+    aImage: $("<img src = assets/images/afterImage9.jpg>"),
     explanation: "Matt Groening was the creative drive behind 'The Simpsons' for over a decade before he came up with the show."
 }];
 
@@ -84,6 +104,7 @@ $(".replay-button").on("click", function(){
 function afterGuessPage(){
     //clears
     $(".question-div").empty();
+    $(".image-div").empty();
     for(var i = 0; i < 4; i++){
         $(".multiple-answer" + i).empty();
         $(".multiple-answer" + i).addClass("hidden");
@@ -93,41 +114,34 @@ function afterGuessPage(){
 
     // show you explanation 
     $(".explanation-div").text(trivia[questionNumber].explanation);
-
-    console.log("question number:" + questionNumber);
-    
+    $(".after-image").append(trivia[questionNumber].aImage);
+   
     //is answer correct?
     if(userPick===trivia[questionNumber].answer){
-        console.log("correct answer");
         $(".after-guess-message").text(afterGuessMessage.correctGuessMessage);
         rightAnswer++;
-        console.log("number of right answers: "+ rightAnswer);
     }
     else if(userPick !== trivia[questionNumber].answer){
-        console.log("wrong answer");
         $(".after-guess-message").text(afterGuessMessage.incorrectGuessMessage);
         wrongAnswer++;
-        console.log("number of wrong answers: " + wrongAnswer);
     }
     else{//needs to be reworked. possibly boolean?
-        console.log("no answer");
         $(".after-guess-message").text(afterGuessMessage.ranOutOfTimeMessage);
         noAnswer++;
     }
     
     //remove data from previous answer
     $(userPick).removeAttr("data-index-number");
-    console.log("user pick should be removed here: " + userPick);
     questionNumber++;
 
     //setTimeout(runningClock,1000);
 
     //show next page, unless last question, then show end page
-    if(questionNumber < (trivia.length - 1) ){
-        setTimeout(giveQuestion, 5000);
+    if(questionNumber < (trivia.length) ){
+        setTimeout(giveQuestion, 2000);
     }
     else{
-        setTimeout(endGame,5000);
+        setTimeout(endGame,2000);
     }
 }
 
@@ -142,19 +156,19 @@ function giveQuestion(){
     $(".explanation-div").empty();
     $(".after-guess-message").empty();
     $(".clock").empty();
+    $(".after-image").empty();
     clearInterval(timer);
-seconds =5;
+    seconds =25;
     //post question
     $(".question-div").html(trivia[questionNumber].question);
-    console.log(trivia[questionNumber].question);
+    //post picture
+    $(".image-div").append(trivia[questionNumber].qImage);
     //post answers
     for(var i = 0; i < 4; i++){
         $(".multiple-answer"+i).text(trivia[questionNumber].possibleAnswers[i]);
-        console.log(trivia[questionNumber].possibleAnswers[i]);
         $(".multiple-answer" + i).removeClass("hidden");
         $(".multiple-answer" + i).addClass("choice");
         $(".multiple-answer"+ i).attr("data-index-number", i);
-        // $(".multiple-answer" + i).data(i);
     }
 
     //load answer page if clock runs out 
@@ -182,15 +196,12 @@ $(document).on("click",".choice",function(){
     userPick = $(this).attr("data-index-number");
     // userPick = $(this).data(i)
     userPick = parseInt(userPick);
-    console.log("user pick:" + userPick);
     // clearTimeout(runningClock);
     clearInterval(timer);
     afterGuessPage();
 });
 
-//try turning off the lcick at end of function, on at the beginning of function to come.
-
-var seconds = 5;
+var seconds = 25;
 var timer;
 
 function showClockRunning(){
@@ -200,13 +211,16 @@ function showClockRunning(){
      if(seconds ===0){
          clearInterval(timer);
          afterGuessPage();
-         seconds=5;
+         seconds=25;
      }
 }
 
 
 //this should start a new game
 function newGame (){
+    $(".number-correct-guess").empty();
+    $(".number-incorrect-guess").empty();
+    
     rightAnswer = 0;
     wrongAnswer = 0;
     noAnswer = 0;
@@ -218,8 +232,14 @@ function endGame(){
     //clear answer page
     $(".explanation-div").empty();
     $(".after-guess-message").empty();
+    $(".clock").empty();
+    $(".after-image").empty();
     
+    $(".number-correct-guess").html("Correct: " + rightAnswer);
+    $(".number-incorrect-guess").html("Incorrect: " + wrongAnswer);
+
     //show replay button
-    $("replay-button").removeClass("hidden");
+    $(".replay-button").removeClass("hidden");
+    console.log("Reached end");
 
 }
